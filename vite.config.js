8eb855d01dd70b216c44c0557400daac7c772e9b
Pablo Porta <pabloportatuimil12@gmail.com>
2026-02-04 18:32:54 +0100

    Style: colors modified.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: "https://portatuimil.github.io/Technical_Blog/",
+    /*base: "https://portatuimil.github.io/Technical_Blog/",*/
+    base: '/Technical_Blog/'
 })
